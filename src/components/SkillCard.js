ac8f03fe65b78feeaf39862e4532f38af93d2835
pedrokpdo <pedromotoehm@gmail.com>
2022-05-18 09:15:20 -0300

    meu primeiro componente customizado

---
+++ src/components/SkillCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { TouchableOpacity, StyleSheet, Text } from 'react-native'
+
+export function SkillCard() {
+    return (
+        <TouchableOpacity key='1' style={styles.buttonskill}>
+            <Text style={[styles.textSkill]}>skill</Text>
+        </TouchableOpacity>
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+    buttonskill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10
+    },
+
+    textSkill: {
+        color: '#fff',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+})
+
