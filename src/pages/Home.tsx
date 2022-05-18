fd7bb9531c6884253372d19fda1c0fb1c53135a8
pedrokpdo <pedromotoehm@gmail.com>
2022-05-18 12:00:20 -0300

    tipando objetos e estado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,22 @@ import React, { useState, useEffect } from 'react';
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+    id:string;
+    name:string;
+}
+
 export function Home() {
     const [newSkill, setNewSkill] = useState('')
-    const [mySkills, setMySkills] = useState([])
+    const [mySkills, setMySkills] = useState<SkillData[]>([])
     const [gretting, setGretting] = useState('')
 
     function handleAddNewSkill() {
-        setMySkills([...mySkills, newSkill])
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+        setMySkills([...mySkills, data])
     }
 
     useEffect(() => {
@@ -40,9 +49,9 @@ export function Home() {
             <Text style={[styles.title, { marginVertical: 50 }]}>My skills</Text>
             <FlatList
                 data={mySkills}
-                keyExtractor={item => item}
+                keyExtractor={item => item.id}
                 renderItem={({ item }) => (
-                    <SkillCard skill={item} />
+                    <SkillCard skill={item.name} />
                 )}>
 
             </FlatList>
